dc96c09eeeca46e8564e455e86981fe7fa7fc180
John Calabrese <jcalabrese@pivotal.io>
2015-11-17 16:02:21 -0500

    [#108253730] only create /tmp if it doesnt exist
    adding -p to mkdir so that
    we only create it if it doesnt exist

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-sudo mkdir /tmp
+sudo mkdir -p /tmp
 sudo chmod 777 -R /tmp
 sudo /etc/init.d/ssh start
 sudo -H -u vcap bash -c '/var/vcap/packages/postgres/bin/postgres -D /var/vcap/store/postgres -h 0.0.0.0 -p 2544'
